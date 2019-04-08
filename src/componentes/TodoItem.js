ad831c2f38951c99af4bb37018fc0fa0c1107c12
zoe606 <rive99erlangga@gmail.com>
2019-04-08 23:01:21 +0700

    todo item

---
+++ src/componentes/TodoItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    // tenary operator
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
+
+  render() {
+    const {id, title } = this.props.todo;
+    return (
+      <div style={ this.getStyle() }>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+         { title }
+        </p>
+      </div>
+    )
+  }
+}
+
+// propTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+export default TodoItem
